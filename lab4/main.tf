6a9db5446405cdfe91647a4d4c9ad68d72c48755
Mellowbricks <skullbatch10493@gmail.com>
2025-10-11 23:22:19 +0530

    last updated on 11-October-2025

--- lab4/main.tf
+++ lab4/main.tf
@@ -1,3 +1,27 @@
+resource "google_storage_bucket" "terra-lab4-bucket" {
+    name     = "terra-lab4-bucket-${random_id.bucket_suffix.hex}"
+    location = "ASIA-SOUTH1"
+    
+    labels = {
+        name        = "terra-lab4-bucket"
+        project     = "terraform"
+        environment = "lab"
+    }
+}
+
+resource "random_id" "bucket_suffix" {
+    byte_length = 8
+}
+
+output "bucket_name" {
+    value = google_storage_bucket.terra-lab4-bucket.name
+}
+
+output "bucket_url" {
+    value = google_storage_bucket.terra-lab4-bucket.url
+}
+
+
 resource "google_compute_instance" "terra-lab4-vm" {
     name         = "terra-lab4-vm"
     machine_type = "e2-micro"
